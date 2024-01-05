7289978fb83fc9638b72e2fc5e8da5ba5a53479d
Radu-Branzei <115561060+Radu-Branzei@users.noreply.github.com>
2024-01-05 14:32:06 +0200

    Added unit tests for game service.

--- Ludo.Server/Ludo.Domain/Entities/Player.cs
+++ Ludo.Server/Ludo.Domain/Entities/Player.cs
@@ -8,5 +8,15 @@ namespace Ludo.Domain.Entities
         public string? ConnectionId { get; set; }
         public List<Piece>? Pieces { get; set; }
         public bool IsReady { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is not Player player)
+                return false;
+
+            return IsReady == player.IsReady &&
+                   Name == player.Name &&
+                   ConnectionId == player.ConnectionId;
+        }
     }
 }
